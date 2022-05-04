d999a40c9e12812d5282da7c8bb5b8fd3339ccf2
Trey Ezell <treyezell@Treys-MacBook-Pro.local>
2022-05-04 12:19:18 -0500

    Final Code Files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace web_pa_Treyzell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
